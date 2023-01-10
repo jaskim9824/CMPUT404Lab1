9dd44cd368087364b3183cd998fb53302bc670fb
jaskim9824 <jaskim9824@gmail.com>
2023-01-09 17:08:43 -0700

    added self query

--- version.py
+++ version.py
@@ -3,4 +3,9 @@ import requests as req
 print(req.__version__)
 
 request = req.get("http://www.google.com/")
-print(request)
+print("Google Home Page: ")
+print(request.text)
+
+script = req.get("https://raw.githubusercontent.com/jaskim9824/CMPUT404Lab1/master/version.py")
+print("Script on Github: ")
+print(script.text)
